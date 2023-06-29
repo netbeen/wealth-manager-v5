6670886f7f2bff4dfe49ca83ef5050e899c18836
Yang Yang <yangyang.jeff@bytedance.com>
2023-06-29 23:23:33 +0800

    feat: create a general middleware

--- src/app/api/team/route.ts
+++ src/app/api/team/route.ts
@@ -1,11 +1,4 @@
-import prisma from '@@/lib/prisma';
-import jwt from 'jsonwebtoken';
 import {NextRequest, NextResponse} from 'next/server'
-import {
-    StatusCodes,
-} from 'http-status-codes';
-import {SESSION_TOKEN_COOKIE_NAME} from "@/constants";
 
 export async function GET(req: NextRequest) {
-    throw new Error('123')
 }
