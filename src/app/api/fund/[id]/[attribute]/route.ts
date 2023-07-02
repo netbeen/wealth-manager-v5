658f84fee76de96efb4127ce92c030860e8d6efc
Yang Yang <yangyang.jeff@bytedance.com>
2023-07-02 23:00:02 +0800

    feat: support cache when fetch fund data

--- src/app/api/fund/[id]/[attribute]/route.ts
+++ src/app/api/fund/[id]/[attribute]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { usingMiddleware } from '@/utils/serverCommon'
+import { kvGet, kvSet, usingMiddleware } from '@/utils/serverCommon'
 import {
   fetchUnitPriceByIdentifier,
   fetchSplitByIdentifier,
@@ -16,29 +16,50 @@ enum AllowedAttribute {
   'dividend' = 'dividend',
 }
 
-const methods: Record<AllowedAttribute, (identifier: string) => Promise<any>> =
-  {
-    [AllowedAttribute['basic-info']]: fetchBasicInfoByIdentifier,
-    [AllowedAttribute['unit-price']]: fetchUnitPriceByIdentifier,
-    [AllowedAttribute['accumulated-price']]: fetchAccumulatedPriceByIdentifier,
-    [AllowedAttribute.split]: fetchSplitByIdentifier,
-    [AllowedAttribute.dividend]: fetchDividendByIdentifier,
-  }
+const method: Record<AllowedAttribute, (identifier: string) => Promise<any>> = {
+  [AllowedAttribute['basic-info']]: fetchBasicInfoByIdentifier,
+  [AllowedAttribute['unit-price']]: fetchUnitPriceByIdentifier,
+  [AllowedAttribute['accumulated-price']]: fetchAccumulatedPriceByIdentifier,
+  [AllowedAttribute.split]: fetchSplitByIdentifier,
+  [AllowedAttribute.dividend]: fetchDividendByIdentifier,
+}
+
+const cacheExpireTime: Record<AllowedAttribute, number> = {
+  [AllowedAttribute['basic-info']]: 3600 * 24 * 7,
+  [AllowedAttribute['unit-price']]: 60,
+  [AllowedAttribute['accumulated-price']]: 60,
+  [AllowedAttribute.split]: 60,
+  [AllowedAttribute.dividend]: 60,
+}
 
 async function handler(
   req: NextRequest,
   { params }: { params: { id: string; attribute: string } }
 ) {
-  const { attribute } = params
+  const { attribute, id } = params
   const validAttribute = Object.values(AllowedAttribute).includes(
     attribute as AllowedAttribute
   )
   if (!validAttribute) {
     throw new Error(`无效的基金 attribute: ${attribute}`)
   }
-  return NextResponse.json(
-    await methods[attribute as AllowedAttribute](params.id)
+  const cacheKey = `${id}_${attribute}`
+  const cachedData = await kvGet<any>(cacheKey)
+  if (cachedData) {
+    return NextResponse.json(await cachedData)
+  }
+  const networkData = {
+    data: await method[attribute as AllowedAttribute](id),
+  }
+  await kvSet(
+    cacheKey,
+    networkData,
+    cacheExpireTime[attribute as AllowedAttribute]
   )
+  return NextResponse.json({
+    ...networkData,
+    kvHit: false,
+  })
 }
 
 export const GET = usingMiddleware(handler)
--- src/utils/serverCommon.ts
+++ src/utils/serverCommon.ts
@@ -2,8 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 import { SESSION_TOKEN_COOKIE_NAME, TEAM_COOKIE_NAME } from '@/constants'
 import { getLoginUserByJwtToken } from '@/utils/user'
 import { StatusCodes } from 'http-status-codes'
-import { User, Team } from '.prisma/client/index'
+import { Team, User } from '.prisma/client/index'
 import prismaClient from '@@/lib/prismaClient'
+import { kv } from '@vercel/kv'
 
 const pathWithoutAuthentication = ['/api/user/login']
 
@@ -55,4 +56,24 @@ export const usingMiddleware = (
       )
     }
   }
+}
+
+export async function kvGet<T>(key: string): Promise<T | null> {
+  return await kv.get<T>(key)
+}
+
+export async function kvSet<T>(
+  key: string,
+  value: T,
+  ex: number // Set the specified expire time, in seconds. https://redis.io/commands/set/
+): Promise<void> {
+  await kv.set<T>(
+    key,
+    {
+      ...value,
+      kvUpdatedAt: new Date().toLocaleString(),
+      kvHit: true,
+    },
+    { ex }
+  )
 }
